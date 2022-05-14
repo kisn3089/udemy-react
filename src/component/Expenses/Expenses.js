1cc7c2536da094ccea2ac6437e692ee32fbe7a2e
insung <96061695+kisn3089@users.noreply.github.com>
2022-05-14 14:22:17 +0900

    add ChartBar

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -20,6 +21,7 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} getYearValue={yearFilter}/>
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -1,18 +1,38 @@
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
+import React, { useState } from 'react'
 
 const NewExpense = (props) => {
+    const [isAdd, setIsAdd] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsAdd(false);
     };
+
+    const startEditing= () => {
+        setIsAdd(true);
+    }
+
+    const stopEditing = () => {
+        setIsAdd(false);
+    }
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isAdd && (
+                <button onClick={startEditing}>Add New Expense</button>
+            )}
+            {isAdd && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditing}
+                />
+            )}
         </div>
     );
 }
